57d0e2dafad73e33254ae47d61a5ba7ae03b6404
Nilson Pontello <nilson@kentik.com>
2026-06-16 16:05:28 -0300

    Added a script to generate provisioning tokens

--- generate-provisioning-token.sh
+++ generate-provisioning-token.sh
@@ -201,6 +201,7 @@ API_ROOT="${API_ROOT#https://}"
 API_ROOT="${API_ROOT#http://}"
 API_ROOT="${API_ROOT%/}"
 URL="https://${API_ROOT}/kagent/v202401/provisioning-tokens"
+BODY=$(build_request_body)
 
 echo "Creating provisioning token..."
 echo "  API:    $API_ROOT"
